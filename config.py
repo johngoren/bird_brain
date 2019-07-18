14a27264cfc1ff175750765a944a1a5949961090
John Gorenfeld <john@gorenfeld.net>
2019-07-18 11:30:50 +0100

    new build

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 PACKAGE = "com.natureguides.birdguide"
 ANDROID_STUDIO_PROJECTS_PATH = os.environ['STUDIO_PROJECT_PATH']
 PLATFORM_TOOLS_PATH = os.environ['PLATFORM_TOOLS_PATH']
@@ -10,12 +12,13 @@ ADB = f'{PLATFORM_TOOLS_PATH}/adb.exe'
 
 NUM_SPECIES_IMAGES = 792
 
+OBB_VERSION = 119
+OBB_FILENAME = f'main.{OBB_VERSION}.{PACKAGE}.obb'
+
 PATH_OBB_SOURCE = f'{PROJECT_DIR}/assets/expansion'
 PATH_OBB_CREATION = f'{PATH_OBB_SOURCE}/{OBB_FILENAME}'
 PATH_OBB_DESTINATION_DIR = f'/storage/emulated/0/Android/obb/{PACKAGE}'
 PATH_OBB_DESTINATION = f'{PATH_OBB_DESTINATION_DIR}/{OBB_FILENAME}'
-OBB_VERSION = 119
-OBB_FILENAME = f'main.{OBB_VERSION}.{PACKAGE}.obb'
 
 HD_ORIGINALS_PATH = f'/Users/johngorenfeld/Dropbox/natureguides/assets/HD/Europe'
 
